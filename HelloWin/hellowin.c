d5eea3bfb25acd1b67e82a1f47e8ad20e4aec077
adamswangg <adams.wangg@gmail.com>
2018-11-08 20:29:06 +0800

    Add HelloWin example.
    The backbone of simple windows program.

---
+++ HelloWin/hellowin.c
@@ -0,0 +1,87 @@
+#include <Windows.h>
+
+static TCHAR WNDCLASSNAME[] = TEXT("HelloWndClass");
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(
+	HINSTANCE		hInstance,
+	HINSTANCE		hPrevInstance,
+	PSTR			szCmdLine,
+	int				iCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("HelloWin");
+	HWND		hwnd;
+	MSG			msg;
+	WNDCLASSEX	wndclass;
+
+	// Init WNDCLASSEX structure
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = WNDCLASSNAME;
+
+	wndclass.hIconSm = LoadIcon(NULL, IDI_HAND);
+
+	if (!RegisterClassEx(&wndclass))
+	{
+		MessageBox(NULL, TEXT("RegisterClassEx failed!"),
+			szAppName, MB_ICONERROR);
+		return 0;
+	}
+	hwnd = CreateWindowEx(
+		0,							// dwExStyle
+		WNDCLASSNAME,				// lpClassName
+		TEXT("The Hello Program"),	// lpWindowName
+		WS_OVERLAPPEDWINDOW,		// dwStyle
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,						// hWndParent
+		NULL,						// hMenu
+		hInstance,					// hInstance
+		NULL);						// lpParam
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC			hdc;
+	PAINTSTRUCT ps;
+	RECT		rect;
+
+	switch (message)
+	{
+	//case WM_CREATE:
+	//	return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		GetClientRect(hwnd, &rect);
+		DrawText(hdc, TEXT("Hello, Windows 98!"), -1, &rect,
+			DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		EndPaint(hwnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
